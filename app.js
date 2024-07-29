27143fc863ac99707cf197f580e4dd71ef79566b
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-29 09:53:17 -0700

    firestore

--- app.js
+++ app.js
@@ -1,55 +1,26 @@
-import { collection, addDoc ,  getDocs } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
-import { db } from "./config.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { db } from "./config.js"
 
-const todoForm = document.querySelector('.form')
-const todoInput = document.querySelector('.input')
-const ul = document.querySelector('ul')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.input')
 
 let arr = []
+form.addEventListener('submit' , async (e)=>{
+    e.preventDefault()
 
-async function getData(){
-  const querySnapshot = await getDocs(collection(db, "todos"));
-  querySnapshot.forEach((doc) => {
-  console.log(doc.data());
-  arr.push(doc.data())
-});
-    console.log(arr);
-    rendersTodo()
-}
-getData()
-function rendersTodo(){
-  ul.innerHTML = ""
-  arr.map((item) => {
-    ul.innerHTML += `<li>${item.todo}</li>`
-  })
-}
+    arr.push({
+        todo : input.value
+    })
 
-todoForm.addEventListener('submit' , async (e)=>{
-  e.preventDefault();
- 
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
 
-  if(todoInput === ''){
-    alert('Please enter a valid todo')
-    return;
-  }
- 
-arr.push({
-  todo : todoInput.value
-})
-
-
-rendersTodo()
-
-
-
-  try {
-    const docRef = await addDoc(collection(db, "todos"), {
-      todo : todoInput.value
-    });
-    todoInput.value = "";
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-   
+            todo : input.value
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+      console.log(arr);
 })
--- config.js
+++ config.js
@@ -1,15 +1,17 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
- 
- const firebaseConfig = {
-   apiKey: "AIzaSyCdBY-uSfyHegP4lgbgpPUjl8udnzWDecs",
-   authDomain: "fir-authentication-9f772.firebaseapp.com",
-   projectId: "fir-authentication-9f772",
-   storageBucket: "fir-authentication-9f772.appspot.com",
-   messagingSenderId: "744029323114",
-   appId: "1:744029323114:web:8f11edf2e6775d63a4ef13",
-   measurementId: "G-ZKSQH73R88"
- };
 
- const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyCdBY-uSfyHegP4lgbgpPUjl8udnzWDecs",
+  authDomain: "fir-authentication-9f772.firebaseapp.com",
+  projectId: "fir-authentication-9f772",
+  storageBucket: "fir-authentication-9f772.appspot.com",
+  messagingSenderId: "744029323114",
+  appId: "1:744029323114:web:8f11edf2e6775d63a4ef13",
+  measurementId: "G-ZKSQH73R88"
+};
+
+
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
