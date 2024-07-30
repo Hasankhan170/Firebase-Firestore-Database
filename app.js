1b78e7164d899d387fc6f5bdd33fdc8b07a0f6b2
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-30 00:51:11 -0700

    data store in arr

--- app.js
+++ app.js
@@ -4,24 +4,38 @@ import { db } from "./config.js"
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
+const ul = document.querySelector('ul')
 
+let arr = []
 
 form.addEventListener('submit' , async (e)=>{
     e.preventDefault()
 
+    arr.push({
+      todo : input.value
+    })
 
 
+    rendersTodos()
     try {
         const docRef = await addDoc(collection(db, "users"), {
-
           todo : input.value
-
-        
         });
-        // console.log(todo);
+        input.value = ""
         console.log("Document written with ID: ", docRef.id);
       } catch (e) {
         console.error("Error adding document: ", e);
       }
       
 })
+
+
+function rendersTodos(){
+  ul.innerHTML = ""
+
+  arr.map((item)=>{
+    ul.innerHTML += `<li>${item.todo}</li>`
+  })
+console.log(arr);
+
+}
