c5d01db1c6366cb73dc29431877a0e39890bc65a
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-02 09:54:24 -0700

    complete

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-import { collection, addDoc ,getDocs ,doc, deleteDoc ,updateDoc } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { collection, addDoc ,getDocs ,doc, deleteDoc ,updateDoc,Timestamp, query,
+  where, orderBy } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 import { db } from "./config.js"
 
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
 const ul = document.querySelector('ul')
+const select = document.querySelector('#select')
+const citiesBtn = document.querySelectorAll('.cities-btn')
+const reset = document.querySelector(".reset");
+
 
 let arr = []
 
+citiesBtn.forEach((btn)=>{
+  btn.addEventListener('click', async (e)=>{
+    arr = []
+    const todosRef = collection(db, "users");
+    const q = query(
+      todosRef,
+      where("city", "==", e.target.innerHTML),orderBy("time", "desc"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      arr.push({ ...doc.data(), id: doc.id });
+    });
+    // console.log(arr);
+    
+    rendersTodos()
+  })
+})
+
+reset.addEventListener('click' , getData)
+
 async function getData(){
-   const querySnapshot = await getDocs(collection(db, "users"));
+  arr = []
+  const q = query(collection(db, "users"), orderBy("time", "desc"));
+  const querySnapshot = await getDocs(q);
    querySnapshot.forEach((doc) => {
    console.log(doc.data());
+   
    arr.push({...doc.data() , id:doc.id})
+   
+   
+   
 });
 
 rendersTodos()
@@ -27,6 +57,8 @@ function rendersTodos(){
     ul.innerHTML += `<li>${item.todo}</li>
     <button class = "deleteBtn">Delete</button>
     <button class = "editBtn">Edit</button>
+    <p>${item.time ? item.time.toDate() : "no time"}</p>
+    <hr/>
     `
   })
 
@@ -53,36 +85,24 @@ function rendersTodos(){
 
     arr[index].todo = updateValue
     rendersTodos()
-    
-
     })
   })
-
-
-  
-
-
-
-
-
 }
 
 
 form.addEventListener('submit' , async (e)=>{
     e.preventDefault()
-
-
-
-
-    
     try {
         const docRef = await addDoc(collection(db, "users"), {
-          todo : input.value
+          todo : input.value,
+          city : select.value,
+          time: Timestamp.fromDate(new Date()),
         });
 
         console.log("Document written with ID: ", docRef.id);
         arr.push({
           todo : input.value,
+          city : select.value,
           id : docRef.id
         }) 
         rendersTodos()
