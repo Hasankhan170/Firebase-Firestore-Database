40461ff0a51cb7d64b81e10bf3c15407afc67dda
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-02 04:01:20 -0700

    delete and edit done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { collection, addDoc ,getDocs } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { collection, addDoc ,getDocs ,doc, deleteDoc ,updateDoc } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 import { db } from "./config.js"
 
 
@@ -8,41 +8,85 @@ const ul = document.querySelector('ul')
 
 let arr = []
 
-async function geteData(){
+async function getData(){
    const querySnapshot = await getDocs(collection(db, "users"));
    querySnapshot.forEach((doc) => {
    console.log(doc.data());
-   arr.push(doc.data())
+   arr.push({...doc.data() , id:doc.id})
 });
 
+rendersTodos()
+
 }
-geteData()
+getData()
 
 function rendersTodos(){
   ul.innerHTML = ""
 
   arr.map((item)=>{
-    ul.innerHTML += `<li>${item.todo}</li>`
+    ul.innerHTML += `<li>${item.todo}</li>
+    <button class = "deleteBtn">Delete</button>
+    <button class = "editBtn">Edit</button>
+    `
   })
-console.log(arr);
+
+  const editBtn = document.querySelectorAll('.editBtn')
+
+  const deleteBtn = document.querySelectorAll('.deleteBtn')
+
+
+  deleteBtn.forEach((btn , index)=>{
+    btn.addEventListener('click', async ()=>{
+      deleteDoc(doc(db, "users", arr[index].id))
+      arr.splice(index,1)
+      rendersTodos()
+    })
+  })
+
+  editBtn.forEach((btn , index)=>{
+    btn.addEventListener('click' , async ()=>{
+      const updateValue = prompt('enter new value')
+      const cityRef = doc(db, 'users', arr[index].id);
+      await updateDoc(cityRef, {
+        todo: updateValue
+    });
+
+    arr[index].todo = updateValue
+    rendersTodos()
+    
+
+    })
+  })
+
+
+  
+
+
+
+
 
 }
+
 
 form.addEventListener('submit' , async (e)=>{
     e.preventDefault()
 
-    arr.push({
-      todo : input.value
-    })
 
 
-    rendersTodos()
+
+    
     try {
         const docRef = await addDoc(collection(db, "users"), {
           todo : input.value
         });
-        input.value = ""
+
         console.log("Document written with ID: ", docRef.id);
+        arr.push({
+          todo : input.value,
+          id : docRef.id
+        }) 
+        rendersTodos()
+        input.value = ""
       } catch (e) {
         console.error("Error adding document: ", e);
       }
